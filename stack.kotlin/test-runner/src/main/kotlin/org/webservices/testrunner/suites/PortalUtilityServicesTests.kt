a1e28e7b0bb5ec0db6d060f6774b5b91670b8d4f
Gerald Summers <geraldsummers@users.noreply.github.com>
2026-07-07 23:28:55 +1000

    Fix homepage dashboard contract

--- stack.kotlin/test-runner/src/main/kotlin/org/webservices/testrunner/suites/PortalUtilityServicesTests.kt
+++ stack.kotlin/test-runner/src/main/kotlin/org/webservices/testrunner/suites/PortalUtilityServicesTests.kt
@@ -6,7 +6,7 @@ import org.webservices.testrunner.framework.*
 
 suspend fun TestRunner.portalUtilityServicesTests() = suite("Portal Utility Service Tests") {
     test("Homepage dashboard loads") {
-        val response = client.getRawResponse("${env.endpoints.portal!!}")
+        val response = client.getRawResponse("http://portal:3000")
         require(response.status == HttpStatusCode.OK) {
             "Homepage dashboard not accessible: ${response.status}"
         }
@@ -14,7 +14,7 @@ suspend fun TestRunner.portalUtilityServicesTests() = suite("Portal Utility Serv
         val body = response.bodyAsText()
         require(
             body.contains("<title data-next-head=\"\">Homepage</title>") &&
-                body.contains("Self-hosted service dashboard") &&
+                body.contains("A highly customizable homepage") &&
                 body.contains("service-card")
         ) {
             "Homepage service dashboard content not detected"
